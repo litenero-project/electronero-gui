486e6d040ee68eb6213a551d5da8a173a4ec01f0
Mark Evans <president@worldvaporexpo.com>
2019-05-01 16:35:14 -0400

    update runner

--- get_libwallet_api.sh
+++ get_libwallet_api.sh
@@ -17,7 +17,7 @@ if [ ! -d $MONERO_DIR/src ]; then
 fi
 git submodule update --remote
 git -C $MONERO_DIR fetch
-git -C $MONERO_DIR checkout 1.0.4
+git -C $MONERO_DIR checkout 1.0.2
                   # up next 1.0.1
 
 # get monero core tag
